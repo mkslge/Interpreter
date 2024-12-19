9bb019f934b217b711028b81a370487d4b097dc1
Mark Seeliger <marklseeliger@gmail.com>
2024-12-19 17:21:46 +0100

    l

--- Lexer/lexer.c
+++ Lexer/lexer.c
@@ -4,3 +4,12 @@
 
 
 char* regex_plus = "[+]";
+char* regex_minus = "[-]";
+char* regex_number = "[:number]";
+char* regex_lparen = "[:(]";
+char* regex_rparen = "[:)]";
+char* regex_multiply = "[:*]";
+char* regex_divide = "[:/]";
+char* regex_equals = "[:=]";
+char* regex_if = "[:if]";
+char* regex_else = "[:else]";
