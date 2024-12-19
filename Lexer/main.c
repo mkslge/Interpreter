5bf6043b1d624e6fbe46c024ff47e53a0ffd3540
Mark Seeliger <marklseeliger@gmail.com>
2024-12-19 16:42:03 +0100

    start

---
+++ Lexer/main.c
@@ -0,0 +1,20 @@
+
+#include <regex.h>
+#include <stdio.h>
+
+int main() {
+    //testing regex
+        regex_t regex;
+        int value = -1;
+
+        value = regcomp(&regex, "[:number]",0);
+        if(!value) {
+            printf("Success");
+        } else {
+            printf("Fail");
+        }
+
+
+
+    return 0;
+}
