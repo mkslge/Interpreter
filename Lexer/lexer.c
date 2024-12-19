4646192c3cb8c9334b93c8c88a61d6f162c4d13a
Mark Seeliger <marklseeliger@gmail.com>
2024-12-19 17:59:31 +0100

    fixed regex

--- Lexer/lexer.c
+++ Lexer/lexer.c
@@ -3,13 +3,23 @@
 
 
 
-char* regex_plus = "[+]";
-char* regex_minus = "[-]";
+char* regex_plus = "+";
+char* regex_minus = "-";
 char* regex_number = "[:number]";
-char* regex_lparen = "[:(]";
-char* regex_rparen = "[:)]";
-char* regex_multiply = "[:*]";
-char* regex_divide = "[:/]";
-char* regex_equals = "[:=]";
-char* regex_if = "[:if]";
-char* regex_else = "[:else]";
+char* regex_lparen = "(";
+char* regex_rparen = ")";
+char* regex_multiply = "*";
+char* regex_divide = "/";
+char* regex_equals = "=";
+char* regex_if = "if";
+char* regex_else = "else";
+char* regex_more_than = ">";
+char* regex_less_than = "<";
+char* regex_more_or_equal = ">=";
+char* regex_less_or_equal = "<=";
+char* regex_equal_comparison = "==";
+char* regex_starting_brace = "{";
+char* regex_ending_brace = "}";
+char* regex_int = "int";
+char* regex_boolean = "bool";
+char* regex_
