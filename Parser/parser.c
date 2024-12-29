efd8a4dfccd7ef2c7be7edc9fa5a6ba2e5cc3196
Mark Seeliger <marklseeliger@gmail.com>
2024-12-29 10:42:55 +0000

    began parser

---
+++ Parser/parser.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "token.c"
+
+
+TokenType lookahead(Token* token) {
+    if(!token) {
+        printf("null\n");
+        return DUMMY_TOKEN;
+    }
+    return token->type;
+}
+
+
+Token* match_token(Token* token, TokenType type) {
+    if(!token) {
+        printf("match_token encountered NULL\n");
+        EXIT_FAILURE;
+    } else if(token->type != type) {
+        printf("wrong token!\n");
+        EXIT_FAILURE;
+    }
+    return token->next;
+}
+
+
+
